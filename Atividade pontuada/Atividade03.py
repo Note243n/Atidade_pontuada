f02a5dcd5a32fe6ab46fc7f1f6c61355c39daffc
Note <tailon.menezes@ba.estudante.senai.br>
2025-03-08 06:47:59 +0000

    Salvando

--- Atividade pontuada/Atividade03.py
+++ Atividade pontuada/Atividade03.py
@@ -3,16 +3,20 @@ os.system('clear') # LIMPANDO O TERMINAL
 
 # SOLICITANDO DADOS 
 
-a = int (input('Informe um valor: '))
-b = int (input('Informe o segundo valor: '))
+a = int(input('Informe um valor: '))
+os.system('clear')
+
+b = int(input('Informe o segundo valor: '))
+os.system('clear')
+
 s = a + b
 m = a * b
 
 # IMPRIMINDO RESULTADO 
 
 if a == b :
-    print(f'O valor {a} e o valor {b} são iguais portanto eles passaram a ser somados dando um resultado de {s} ')
+    print(f'\nO valor {a} e o valor {b} são iguais portanto eles passaram a ser somados dando um resultado de\n > {s} \n')
 else :
-    print(f'O valor {a} e o valor {b} são diferentes portanto eles passaram a ser multiplicados dando um resultado de {m} ')
+    print(f'O valor {a} e o valor {b} são diferentes portanto eles passaram a ser multiplicados dando um resultado de\n > {m} \n ')
 
 # FIM DO PROGRAMA
