9f743a4fb25603090e052dcdd3f512f0cc044a90
Note <tailon.menezes@ba.estudante.senai.br>
2025-03-08 08:41:14 +0000

    Salvando

--- Atividade pontuada/Atividade05.py
+++ Atividade pontuada/Atividade05.py
@@ -1,34 +1,31 @@
 import os
-os.system('clear')
+os.system('clear') # LIMPANDO O TERMINAL 
 
+# SOLICITANDO DADOS 
 
-print("""
-1 + 
-2 -
-3 *
-4 /    
-""")
+operacao = input("Informe a operação (+, -, *, /): ")
 
+A = int (input("Informe o valor de A: "))
+B = int (input("Informe o valor de B: "))
 
+# IMPRIMINDO RESULTADO 
 
+if operacao == "+":
+    resultado = A + B
+elif operacao == "-":
+    resultado = A - B
+elif operacao == "*":
+    resultado = A * B
+elif operacao == "/":
+    if B != 0:
+        resultado = A / B
+    else:
+        print("Erro: Divisão por zero!")
+        exit()
+else:
+    print("Erro: Operação inválida!")
+    exit()
 
+print(f"O resultado da operação é: {resultado}")
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# FIM DO PROGRAMA
